127fc9dcbade62cf45d107c6c878f6622a286d5a
SOUROV <sourovsarker005@gmail.com>
2025-11-18 22:31:09 +0600

    feat: database connection and module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { FileModule } from './file/file.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    AuthModule,
+    UserModule,
+    FileModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
